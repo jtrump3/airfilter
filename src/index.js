97060d83ce2ad38df11f968a071e7f50a2a1d925
Byron Hall <byron.hall@searshc.com>
2018-11-16 12:26:28 -0600

    added some state properties

--- src/index.js
+++ src/index.js
@@ -9,12 +9,28 @@ import {
 
 import {GlobalConstants} from "./GlobalConstants";
 
-const Index = () => {
-  console.log(AIR_FILTER_WIDTHS);
-  console.log(AIR_FILTER_HEIGHTS);
-  console.log(AIR_FILTER_THICKNESSES);
-  console.log(GlobalConstants);
-  return <div>Hello React!</div>;
-};
-
+class Index extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      shortSideSelected: false,
+      longSideSelected: false,
+    };
+  }
+  startOver(){
+    this.setState({
+      shortSideSelected: false,
+      longSideSelected: false,
+    });
+  }
+  render(){
+    console.log(AIR_FILTER_WIDTHS);
+    console.log(AIR_FILTER_HEIGHTS);
+    console.log(AIR_FILTER_THICKNESSES);
+    console.log(GlobalConstants);
+    return (
+    <div>Hello React!</div>
+    );
+  }
+}
 ReactDOM.render(<Index />, document.getElementById("app"));
