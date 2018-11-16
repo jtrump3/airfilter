6c3d3fc9cd77d09e25d848bdf380729c0df82625
Byron Hall <byron.hall@searshc.com>
2018-11-16 12:26:28 -0600

    mock data

---
+++ src/GlobalConstants.js
@@ -0,0 +1,14 @@
+export const GlobalConstants = {
+    AIR_FILTER: {
+        ATTRIBUTES: {
+            ONE: "Merv ratings:",
+            TWO: "Ideal for:",
+            THREE: "Removes:",
+            FOUR: "Life:",
+        },
+        OFFER: "Eligible for FREE Standard Shipping with Automatic Reorder Today!",
+        DETAILS: "Details",
+        VIEW: "View more details",
+        QUANTITY: "Qty",
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {
+  AIR_FILTER_WIDTHS,
+  AIR_FILTER_HEIGHTS,
+  AIR_FILTER_THICKNESSES
+} from "./MockData"
+
+import {GlobalConstants} from "./GlobalConstants";
+
 const Index = () => {
+  console.log(AIR_FILTER_WIDTHS);
+  console.log(AIR_FILTER_HEIGHTS);
+  console.log(AIR_FILTER_THICKNESSES);
+  console.log(GlobalConstants);
   return <div>Hello React!</div>;
 };
 
